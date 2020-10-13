68740f1bebb39df662ce810c6d0819b411b2b016
Tfangz <tfangz@ymail.com>
2020-10-13 23:51:02 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,5 +3,7 @@
 # multiple versions of kdb+
 # https://code.kx.com/q/kb/versions/
 export QLIC=/home/toby/q
-alias    q='export QHOME=/home/toby/q/v4.0; rlwrap -r $QHOME/l64/q'
-alias  q32='export QHOME=/home/toby/q/v3.6; rlwrap -r $QHOME/l32/q'
+export QHOME=/home/toby/q/v4.0
+
+# alias    q='export QHOME=/home/toby/q/v4.0; rlwrap -r $QHOME/l64/q'
+# alias  q32='export QHOME=/home/toby/q/v3.6; rlwrap -r $QHOME/l32/q'
